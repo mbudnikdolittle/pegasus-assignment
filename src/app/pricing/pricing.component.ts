8dbd08e37ad49246fac9f9e0c4986ca7e89c098f
mbudnikdolittle <mbudnik@deloittece.com>
2024-10-29 11:34:29 +0100

    add pricing component with header

---
+++ src/app/pricing/pricing.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  templateUrl: './pricing.component.html',
+  styleUrl: './pricing.component.scss',
+  imports: [CommonModule],
+})
+export class PricingComponent {}
